1fb1afd23031fe0416cbe94ea96fa9e4e2b9bf9b
homeworkdome <homeworkdome@outlook.com>
2023-04-24 17:05:21 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection/Main.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, "Выберите элементы");
+            var ductList = new List<Duct>();
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Element element = doc.GetElement(selectedElement);
+                if(element is Duct)
+                {
+                    Duct oDuct = (Duct)element;
+                    ductList.Add(oDuct);
+                }
+            }
+            TaskDialog.Show("Selection", $"Количество: {ductList.Count}");
+            return Result.Succeeded;
+        }
+    }
+}
